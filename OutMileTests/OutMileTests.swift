284c75b1fb82bbfa1d8a6e53648b4416233c2a6d
89Explorer <leral3@naver.com>
2024-12-10 17:04:25 +0900

    Initial Commit

---
+++ OutMileTests/OutMileTests.swift
@@ -0,0 +1,17 @@
+//
+//  OutMileTests.swift
+//  OutMileTests
+//
+//  Created by 권정근 on 12/10/24.
+//
+
+import Testing
+@testable import OutMile
+
+struct OutMileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
